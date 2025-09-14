c40a7ec510f0c3e7c889a95f27a77e158aa1ad33
garima2543 <garimac2543@gmail.com>
2025-09-14 23:19:37 +0530

    Add files via upload

---
+++ streamlit_app.py
@@ -0,0 +1,23 @@
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "7a2c37f7-709b-4b82-ad51-2d0f015202d1",
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "",
+   "name": ""
+  },
+  "language_info": {
+   "name": ""
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 5
+}
